cb3f8f1eebca1f5159b04b89e8d3fc6e521c2a3b
Xudoberdi1107 <tojiboyevxudoberdi1107@gmail.com>
2024-09-22 22:38:02 +0500

    start 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 function App() {
+  const start = 4;
   return (
     <div className="">
       <h1 className="text-red-500">loerm</h1>
 
-      <p>lorem</p>
+      <p>lorem {}</p>
     </div>
   );
 }
